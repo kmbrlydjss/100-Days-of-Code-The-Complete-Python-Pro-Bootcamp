770d955497de0f0d5cc59c76805eb05993f32a34
kmbrlydjss <kimberlysoliva9@gmail.com>
2025-08-02 20:53:10 +0800

    Create Practice: Water, Fire, Grass.py

---
+++ Day 4/Practice: Water, Fire, Grass.py
@@ -0,0 +1,50 @@
+water = '''
+     ▒▒▒▒▒▒
+---'  ▒▒▒▒▒▒))
+     ▒▒▒▒▒▒))
+    ▒▒▒▒▒▒▒))
+    ▒▒▒▒▒▒▒)
+---.__(____))
+'''
+fire = '''
+     (  .      )
+---'  )  ) )   )🔥
+     (  ( . )🔥 )
+    (   )  .  )🔥
+     )🔥 (  ( . ) 
+---.__(____🔥__)
+'''
+grass = '''
+     ,--./,-.
+---' /     🌿  \\
+     |   🌿🌿   |
+     \\   🌿   /
+      `._,._.'
+---.__(______)
+'''
+
+game_image = [water, fire, grass]
+
+import random
+
+user = int(input("What do you choose? Type 0 for Water, 1 for Fire or 2 for Grass "))
+if user >= 0 or user <= 2:
+    print(game_image[user])
+
+computer = random.randint(0, 2)
+print("Computer chose: ")
+print(game_image[computer])
+
+
+if user >= 3 or user < 0:
+    print("You typed a wrong number. You lose!")
+elif user == 0 and user == 2:
+    print("You win!")
+elif user == 0 and user == 2:
+    print("You lose!")
+elif user < computer:
+    print("You win!")
+elif computer < user:
+    print("You lose!")
+elif user == computer:
+    print("It's a Draw")
